2fe43cb7217ed17b0f25f03efd8a0437256cc552
HariomYadav18 <hariomchelsea@gmail.com>
2025-01-24 01:13:39 +0530

    updated css and js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.getElementById('addTaskButton').addEventListener('click', addTask);
+document.getElementById('taskInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        addTask();
+    }
+});
 
 function addTask() {
     const taskInput = document.getElementById('taskInput');
@@ -11,14 +16,15 @@ function addTask() {
 
     const taskList = document.getElementById('taskList');
     const li = document.createElement('li');
-
     li.textContent = taskText;
+
     li.addEventListener('click', () => {
         li.classList.toggle('completed');
     });
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('delete-button'); // Added class for styling
     deleteButton.addEventListener('click', (e) => {
         e.stopPropagation(); // Prevent triggering the li click event
         taskList.removeChild(li);
